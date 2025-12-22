0f1f20c843f0168c9cdd4c4620d8ed989852a801
Daniel <jilek.d@northeastern.edu>
2025-12-21 18:39:58 -0600

    Allow onrender hosting site

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     'http://localhost:5173',
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'https://libro-de-los-juegos.onrender.com'
 ]
 
 module.exports = allowedOrigins
