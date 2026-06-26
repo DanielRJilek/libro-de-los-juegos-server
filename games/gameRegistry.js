12c9246025ca4cbf5b750faea89576b8086bce11
Daniel Jilek <jilek.d@northeastern.edu>
2026-06-25 19:29:40 -0500

    Use dashes

--- games/gameRegistry.js
+++ games/gameRegistry.js
@@ -1,11 +1,13 @@
-const Laquet = require('./laquet/Laquet');
+const Laquet = require('./Laquet');
+const CabeQuinal = require('./CabeQuinal');
 
 const MODELS = {
   laquet: Laquet,
+  cabequinal: CabeQuinal,
 };
 
 function getGameModel(gameInstance) {
-  const GameClass = MODELS[gameInstance.title?.toLowerCase()];
+  const GameClass = MODELS[gameInstance.title?.toLowerCase().replace(/-/g, '')];
   if (!GameClass) throw new Error(`Unknown game: ${gameInstance.title}`);
   return new GameClass(gameInstance);
 }
